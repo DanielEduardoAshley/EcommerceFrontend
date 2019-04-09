3ee01bb5ad35bc3171636f04107507343947ab34
daniel ashley <danielashley@pursuit.org>
2019-04-09 17:51:10 -0400

    Updated Products Page

--- src/components/productpageview/productpageview.js
+++ src/components/productpageview/productpageview.js
@@ -1,52 +1,96 @@
-import React, { Component } from 'react';
-import './productpageview.css'
+import React from 'react';
+import { Link, HashRouter } from 'react-router-dom';
+import './productpageview.css';
 import { Button } from 'reactstrap';
 import Productlistingscard from '../productlistingscard/productlistingscard'
 import Sidedashboard from '../sidedashboard/sidedashboard'
+import instance from '../../services/axios'
 
-class ProductPageView extends React.Component{
-render(){
-    return (
-        <>
-    <div className="productrow">
-        {/* <Productlistingscard/> */}
-                <div className="w3-row-padding " style={{"margin" :"0 -16px"}}>
-                    <div className="">
-                        <img src={require('./Crab_Nebula.jpg')} style={{"width" :"100%"}} alt="Northern Lights" className="w3-margin-bottom" className='image'></img>
-                    </div>
-            {/* <div className="w3-half">
-              <img src="/w3images/nature.jpg" style={{"width":"100%"}} alt="Nature" className="w3-margin-bottom"></img>
-          </div> */}
-                </div>
-        <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up"></i>  Like</button> 
-        <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment"></i>  Comment</button>  
+class ProductPageView extends React.Component {
+    state = {
+        productinfo: [],
+        prodsOfSeller: [],
+        sellerid: 0,
+    }
 
-         
-    </div>
-    <br></br>
- 
+    componentDidMount() {
+        const getid = this.props.location.pathname.split('/')
+        const id = getid[getid.length - 1]
+        instance.get(`product/${id}/product`)
+            .then((response) => {
+                console.log('success', response)
+                this.setState({
+                    productinfo: (this.state.productinfo || []).concat(response.data.response)
+                })
+            })
+            .then(() => {
+                return instance.get(`product/${this.state.productinfo[0].seller_id}/products`)
 
-    <div className="productdescription">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,Lorem ipsum dolor sit amet, consectetur adipisicing elit, </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,Lorem ipsum dolor sit amet, consectetur adipisicing elit, </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,Lorem ipsum dolor sit amet, consectetur adipisicing elit, </p>
-    </div>
-        
-
-
-        <div className='featuredproductsrow'>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        <div className='column-2'><img src={require('./Crab_Nebula.jpg')} className='productimg'></img></div>
-        </div>
-        </>
-    )
+            })
+            .then((response) => {
+                console.log('gotprods', response)
+                this.setState({
+                    prodsOfSeller: (this.state.prodsOfSeller || []).concat(response.data.response)
+                }, () => {
+                    console.log(this.state)
+                })
+            })
+    }
+goToSeller=()=>{
+    
+       this.props.history.push(`/searchview/${this.state.productinfo[0].seller_id}`)
+  
 }
+
+    render() {
+        console.log(this.props)
+        console.log(this.state)
+        
+       
+        
+        return (
+            <>
+            
+            <button className='gotoseller'onClick={this.goToSeller}>Go To Seller's Page</button> 
+                <div className="productrow">
+                    {/* <Productlistingscard/> */}
+                    {
+                        this.state.productinfo.map((e, i) => {
+                            return (<><div className="w3-row-padding " style={{ "margin": "0 -16px" }} key={i}>
+                                <div className="">
+                                    <h4>{e.name}</h4> 
+                                    <h1>{`$${e.price}`}</h1>
+                                    <img src={e.images || require('./Crab_Nebula.jpg')} style={{ "width": "100%" }} alt="Northern Lights" className="w3-margin-bottom" className='image'></img>
+                                </div>
+                            </div>
+                                <div className="productdescription"></div>
+                                <p>{e.description} </p>
+
+                            </>)
+                        })
+                    }
+
+                    <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom feedback"><i className="fa fa-thumbs-up"></i>  Like</button>
+                    <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom feedback"><i className="fa fa-comment"></i>  Comment</button>
+
+                </div>
+                <br></br>
+
+                <div className="seller">More From This Seller</div>
+                <div className='featuredproductsrow'>
+                    {
+                        this.state.prodsOfSeller.map((e, i) => {
+
+                            return <div className='column-2' key={i}><img src={e.images || require('./Crab_Nebula.jpg')} className='productimg' ></img>
+                                     <span className="tooltiptext">{e.name}</span>
+                                   </div>
+
+                        })
+                    }
+                </div>
+            </>
+        )
+    }
 }
 
 export default ProductPageView
